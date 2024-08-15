1ffe2a2ec263e2f470a0ab3d39b92f05b62a8b74
Nikhil Lohar <135537329+Nikhilslohar543@users.noreply.github.com>
2024-08-15 13:34:15 +0530

    Add files via upload

---
+++ Classroom_Mgmt_System/src/main/java/com/tka/ClassroomMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomMgmtApplication.class, args);
+	}
+
+}
